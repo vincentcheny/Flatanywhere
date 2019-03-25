4a651f38682980fabe73c9bdf1e6c1cef549a814
vincent <1155107977@link.cuhk.edu.hk>
2019-03-25 14:31:50 +0800

    fix websocket

--- server_part/routes/index.js
+++ server_part/routes/index.js
@@ -1,11 +1,71 @@
 var express = require("express");
 var mysql = require("../dao/dbConnect.js");
 var router = express.Router();
+var http = require("http").Server(express);
+var io = require("socket.io")(http);
 
 /*GET the user account !*/
 // router.get('/',function (req,res){
 //     res.redirect('/home');
 // });
+
+// Define/initialize our global vars
+var notes = []; // also called events
+var isInitNotes = false;
+var socketCount = 0;
+
+io.sockets.on("connection", function(socket) {
+  // Socket has connected, increase socket count
+  socketCount++;
+  // Let all sockets know how many are connected
+  io.sockets.emit("users connected", socketCount);
+
+  socket.on("disconnect", function() {
+    // Decrease the socket count on a disconnect, emit
+    socketCount--;
+    io.sockets.emit("users connected", socketCount);
+  });
+
+  socket.on("new note", function(data) {
+    // New note added, push to all sockets and insert into db
+    notes.push(data);
+    io.sockets.emit("new note", data);
+    // Use node's db injection format to filter incoming data
+    mysql.query(
+      "INSERT INTO SmartLock VALUES (?,?,?,?,?,?,?,?,?,?)",
+      data.a,
+      data.b,
+      data.c,
+      data.d,
+      data.e,
+      data.f,
+      data.g,
+      data.h,
+      data.i,
+      data.j
+    );
+  });
+
+  // Check to see if initial query/notes are set
+  if (!isInitNotes) {
+    // Initial app start, run db query
+    mysql
+      .query("SELECT * FROM SmartLock")
+      .on("result", function(data) {
+        // Push results onto the notes array
+        notes.push(data);
+      })
+      .on("end", function() {
+        // Only emit notes after query has been completed
+        socket.emit("initial notes", notes);
+      });
+
+    isInitNotes = true;
+  } else {
+    // Initial notes already exist, send out
+    socket.emit("initial notes", notes);
+  }
+});
 
 /* GET home page. */
 router
